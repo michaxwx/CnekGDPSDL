0f0e2ced492dc815d135f359214d2f9f0c852a50
michaxwx <thiswillavoidcant@gmail.com>
2026-03-28 21:22:13 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,9 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import RatedUnratedList from './pages/RatedUnratedList.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/rated-unrated', component: RatedUnratedList },
 ];
